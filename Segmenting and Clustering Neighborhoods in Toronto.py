12c99143185ecc48cc81f8d4e2a39f4e8fc9f791
ikedoh8 <43286911+ikedoh8@users.noreply.github.com>
2021-07-16 16:14:43 +0900

    Add files via upload

---
+++ Segmenting and Clustering Neighborhoods in Toronto.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Segmenting and Clustering Neighborhoods in Toronto
+
+# In[3]:
+
+
+get_ipython().system('pip install beautifulsoup4')
+get_ipython().system('pip install lxml')
+import requests # library to handle requests
+import pandas as pd # library for data analsysis
+import numpy as np # library to handle data in a vectorized manner
+import random # library for random number generation
+
+get_ipython().system('conda install -c conda-forge geopy --yes ')
+from geopy.geocoders import Nominatim # module to convert an address into latitude and longitude values
+
+# libraries for displaying images
+from IPython.display import Image 
+from IPython.core.display import HTML 
+
+
+from IPython.display import display_html
+import pandas as pd
+import numpy as np
+    
+# tranforming json file into a pandas dataframe library
+from pandas.io.json import json_normalize
+
+get_ipython().system('conda install -c conda-forge folium=0.5.0 --yes')
+import folium # plotting library
+from bs4 import BeautifulSoup
+from sklearn.cluster import KMeans
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+print('Folium installed')
+print('Libraries imported.')
+
+
+# In[10]:
+
+
+source = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
+soup=BeautifulSoup(source,'lxml')
+print(soup.title)
+from IPython.display import display_html
+tab = str(soup.table)
+display_html(tab,raw=True)
+
+
+# In[5]:
+
+
+dfs = pd.read_html(tab)
+df=dfs[0]
+df.head()
+
+
+# In[ ]:
+
+
+
+
